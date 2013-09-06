ca599f64f9362e3904507dd5124302ea345b2ec8
papalozarou <github@lozworld.com>
2013-09-06 18:16:40 +0200

    add notes on content loaded in via ajax
    Added event listener to document to process any content added via ajax
    and regenerate the notes tab.

--- script/loznotes.js
+++ script/loznotes.js
@@ -1,5 +1,4 @@
-// Metaframe is an easy to use annotation layer for converying information about wireframes, mockups and design comps. Modified by Loz Gray
-// Based on Elliance's metaframe http://github.com/elliance/metaframe
+// loznotes is a notation layer for conveying information about wireframes, mockups and design comps. Originally a fork, this now a complete rewrite of Elliance's excellent Metaframe work (http://github.com/elliance/metaframe).
 // ©2013 Loz Gray – Creative Commons Attribution Sharealike 3.0 Unported http://creativecommons.org/licenses/by-sa/3.0/
 
 $(function () {
@@ -14,14 +13,58 @@ $(function () {
 	// get document URL and parse string from first '?' character, then split by '='
 	var subStrings = window.location.search.substring(1).split('=');
 	
+	// check substring and create notes
+	function checkSubstring () {
+		// if no substring generate notes
+		if (subStrings.length <= 1 || subStrings[1] === 'hidden' || subStrings[1] === 'hide') {
+			createNotes();
+			if (subStrings.length === 1) {
+				$('#note-anchors-display-toggle').attr('checked','checked');
+			}
+		}
+	}
+	
 	// defines function to create notes and notation pane
-	function initiate () {
+	function createNotes () {
+		// check if the notations and tab pane already exist, if so remove
+		if ($('.notes-tab-pane').length > 0) {
+			$('.notes-anchor').add('.notes-tab-control').add('.notes-tab-pane').remove();
+		}
+		
 		// grab all elements with [data-notation]
 		notes = $('[data-notation]');
+		
+		createNotesTab(notes);
+		createNoteAnchors(notes);
+		
+		// grab inserted note anchors
+		var notesAnchors = $('.notes-anchor');
 	
+		// open tab pane
+		$(notesTabControl).add(notesAnchors).click(function (e) {
+			notesTabControl.toggleClass('is-open');
+			notesTabPane.toggleClass('is-open');
+	
+			e.preventDefault();
+		});
+	
+		// turn note anchors on or off
+		$('#note-anchors-display-toggle').click(function () {
+			notesAnchors.toggleClass('notes-hide');
+		});
+	
+		// hide note anchors by default 
+		if (subStrings[1] === 'hidden' || subStrings[1] === 'hide') {
+			$('#notes-anchors-display-toggle').removeAttr('type');
+			notesAnchors.addClass('notes-hide');
+		}
+	}
+	
+	// create the notes tab
+	function createNotesTab (notes) {
 		// generate notes tab
 		notesTabControl = $('<a/>', {'class': 'notes-tab-control',href: '#'}).appendTo('body');
-	
+
 		// generate notes panel
 		notesTabPane = $('<section/>',{'class':'notes-tab-pane'}).appendTo('body');
 
@@ -31,48 +74,28 @@ $(function () {
 		// generate notes panel static content and insert into notes panel
 		var structure = $('<form class="form-notes-display"><input type="checkbox" id="note-anchors-display-toggle" />Show note markers</form><h2>Page Notes</h2>');
 		structure.appendTo(notesTabPane);
-	
+
 		// generate notes list container within notes panel
 		var notesList = $('<dl/>',{'class':'notes-list'}).appendTo(notesTabPane);
-	
+
 		// number and add the notes to the note list
 		for (var i = 0; i < notes.length; i++) {
 			notesList.append('<dt class="note-count note-count-' + (i + 1) + '">' + (i + 1) + '</dt>' + '<dd class="note-body note-body-' + (i + 1) + '">' + $(notes[i]).data('notation') + '</dd>');
 		}
+	}
 	
+	// create the note anchors
+	function createNoteAnchors (notes) {
 		// add note numbers to elements with [data-notation]
-		for (i = 0; i < notes.length; i++) {
+		for (var i = 0; i < notes.length; i++) {
 			$(notes[i]).prepend('<span class="notes-anchor"><figure>' + (i + 1) + '</figure></span>');
 		}
 	}
 	
-	// if no substring generate notes
-	if (subStrings.length <= 1 || subStrings[1] === 'hidden' || subStrings[1] === 'hide') {
-		initiate();
-		if (subStrings.length === 1) {
-			$('#note-anchors-display-toggle').attr('checked','checked');
-		}
-	}
+	checkSubstring();
 	
-	// grab inserted note anchors
-	var notesAnchors = $('.notes-anchor');
-	
-	// open tab pane
-	$(notesTabControl).add(notesAnchors).click(function (e) {
-		notesTabControl.toggleClass('is-open');
-		notesTabPane.toggleClass('is-open');
-	
-		e.preventDefault();
+	// if anything is loaded into the page via ajax, re-create the notes
+	$(document).ajaxComplete(function () {
+		checkSubstring();
 	});
-	
-	// turn note anchors on or off
-	$('#note-anchors-display-toggle').click(function () {
-		notesAnchors.toggleClass('notes-hide');
-	});
-	
-	// hide substrings by default 
-	if (subStrings[1] === 'hidden' || subStrings[1] === 'hide') {
-		$('#notes-anchors-display-toggle').removeAttr('type');
-		notesAnchors.addClass('notes-hide');
-	}
 });
