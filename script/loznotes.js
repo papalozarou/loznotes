3641f36540caf334b335b39fff67d5268b13594e
papalozarou <loz@Lab-7-MBA-13.local>
2013-12-06 10:38:18 +0100

    Properly working switches and body note
    Switches have changed to 'loznotes=' format and will work properly with
    other substrings (such as Codekit cachebuster).
    
    Also added ability to place a note on the body element, that can be
    used to describe the page.

--- script/loznotes.js
+++ script/loznotes.js
@@ -1,98 +1,134 @@
-// loznotes is a notation layer for conveying information about wireframes, mockups and design comps. Originally a fork, this now a complete rewrite of Elliance's excellent Metaframe work (http://github.com/elliance/metaframe).
-// ©2013 Loz Gray – Creative Commons Attribution Sharealike 3.0 Unported http://creativecommons.org/licenses/by-sa/3.0/
-
+// -----------------------------------------------------------------------------
+// loznotes is a notation layer for conveying information about wireframes, 
+// mockups and design comps. Originally a fork, this now a complete rewrite of 
+// Elliance's excellent Metaframe work (http://github.com/elliance/metaframe).
+//
+// ©2013 Loz Gray – Creative Commons Attribution Sharealike 3.0 Unported 
+// http://creativecommons.org/licenses/by-sa/3.0/
+//
+// Dependancies:
+//
+// jquery 2.x.x
+// loznotes.css
+// -----------------------------------------------------------------------------
 $(function () {
 	// some global variables
 	var notes;
+	var bodyNote;
+	
 	var notesTabControl;
 	var notesTabPane;
+	var notesAnchors;
 	
 	// defined here for ease
 	var title = $(this).attr('title');
 	
-	// get document URL and parse string from first '?' character, then split by '='
-	var subStrings = window.location.search.substring(1).split('=');
+	// get document URL and parse strings from first '?' character, 
+	// then split by '&'
+	var subStrings = window.location.search.substring(1).split('&');
 	
 	// check substring and create notes
 	function checkSubstring () {
-		// if no substring generate notes
-		if (subStrings.length <= 1 || subStrings[1] === 'hidden' || subStrings[1] === 'hide') {
+		// check for switches
+		if ($.inArray('loznotes=hidden',subStrings) > -1 || $.inArray('loznotes=hide',subStrings) > -1) {
 			createNotes();
-			if (subStrings.length === 1) {
-				$('#note-anchors-display-toggle').attr('checked','checked');
-			}
+			
+			// hide note anchors by default
+			notesAnchors.addClass('loznotes__anchor--is-hidden');
+		} else if ($.inArray('loznotes=off',subStrings) > -1) {
+			return false;
+		} else {
+			createNotes();
+			
+			// show note anchors by default
+			$('#loznotes__form__display-toggle').attr('checked','checked');
 		}
 	}
 	
-	// defines function to create notes and notation pane
+	// create notes and notation pane
 	function createNotes () {
-		// check if the notations and tab pane already exist, if so remove
-		if ($('.notes-tab-pane').length > 0) {
-			$('.notes-anchor').add('.notes-tab-control').add('.notes-tab-pane').remove();
-		}
+		checkExisting();
 		
 		// grab all elements with [data-notation]
 		notes = $('[data-notation]');
 		
-		createNotesTab(notes);
-		createNoteAnchors(notes);
+		// check for notation on body, create notes, tab and anchors
+		checkBodyNote();
+		createNotesTab();
+		createNoteAnchors();
 		
 		// grab inserted note anchors
-		var notesAnchors = $('.notes-anchor');
+		notesAnchors = $('.loznotes__anchor');
+	}
 	
-		// open tab pane
-		$(notesTabControl).add(notesAnchors).click(function (e) {
-			notesTabControl.toggleClass('is-open');
-			notesTabPane.toggleClass('is-open');
+	// check if the notations and tab pane already exist, if so remove
+	function checkExisting () {
+		if ($('.loznotes__tab-pane').length > 0) {
+			$('.loznotes__anchor').add('.loznotes__tab-control').add('.loznotes__tab-pane').remove();
+		}
+	}
 	
-			e.preventDefault();
-		});
-	
-		// turn note anchors on or off
-		$('#note-anchors-display-toggle').click(function () {
-			notesAnchors.toggleClass('notes-hide');
-		});
-	
-		// hide note anchors by default 
-		if (subStrings[1] === 'hidden' || subStrings[1] === 'hide') {
-			$('#notes-anchors-display-toggle').removeAttr('type');
-			notesAnchors.addClass('notes-hide');
+	// check to see if the body has a data-notation
+	function checkBodyNote () {
+		if ($('body').data('notation')) {
+			// seperate body note
+			bodyNote = $(notes[0]);
+			
+			// remove body note from array
+			notes = notes.splice(1);
 		}
 	}
 	
 	// create the notes tab
-	function createNotesTab (notes) {
+	function createNotesTab () {
 		// generate notes tab
-		notesTabControl = $('<a/>', {'class': 'notes-tab-control',href: '#'}).appendTo('body');
+		notesTabControl = $('<a/>', {'class': 'loznotes__tab-control',href: '#'}).appendTo('body');
 
 		// generate notes panel
-		notesTabPane = $('<section/>',{'class':'notes-tab-pane'}).appendTo('body');
+		notesTabPane = $('<section/>',{'class':'loznotes__tab-pane'}).appendTo('body');
 
 		// get page title and insert into notes panel
 		notesTabPane.html('<h1>' + title + '</h1>');
 	
 		// generate notes panel static content and insert into notes panel
-		var structure = $('<form class="form-notes-display"><input type="checkbox" id="note-anchors-display-toggle" />Show note markers</form><h2>Page Notes</h2>');
+		var structure = $('<form class="loznotes__form"><input type="checkbox" id="loznotes__form__display-toggle" />Show note markers</form><h2>Page Notes</h2>');
 		structure.appendTo(notesTabPane);
 
-		// generate notes list container within notes panel
-		var notesList = $('<dl/>',{'class':'notes-list'}).appendTo(notesTabPane);
+		if (bodyNote) {
+			notesTabPane.append('<p class="loznotes__page-description">' + bodyNote.data('notation') + '</p>');
+		}
 
+		// generate notes list container within notes panel
+		var notesList = $('<dl/>',{'class':'loznotes__list'}).appendTo(notesTabPane);
+		
 		// number and add the notes to the note list
 		for (var i = 0; i < notes.length; i++) {
-			notesList.append('<dt class="note-count note-count-' + (i + 1) + '">' + (i + 1) + '</dt>' + '<dd class="note-body note-body-' + (i + 1) + '">' + $(notes[i]).data('notation') + '</dd>');
+			notesList.append('<dt class="loznote__count loznote__count-' + (i + 1) + '">' + (i + 1) + '</dt>' + '<dd class="loznote__body loznote__body-' + (i + 1) + '">' + $(notes[i]).data('notation') + '</dd>');
 		}
 	}
 	
 	// create the note anchors
-	function createNoteAnchors (notes) {
+	function createNoteAnchors () {
 		// add note numbers to elements with [data-notation]
 		for (var i = 0; i < notes.length; i++) {
-			$(notes[i]).prepend('<span class="notes-anchor"><figure>' + (i + 1) + '</figure></span>');
+			$(notes[i]).prepend('<span class="loznotes__anchor"><figure>' + (i + 1) + '</figure></span>');
 		}
 	}
 	
 	checkSubstring();
+	
+	// open tab pane
+	$(notesTabControl).add(notesAnchors).click(function (e) {
+		notesTabControl.toggleClass('loznotes__tab-control--is-active');
+		notesTabPane.toggleClass('loznotes__tab-pane--is-active');
+
+		e.preventDefault();
+	});
+
+	// turn note anchors on or off
+	$('#loznotes__form__display-toggle').click(function () {
+		notesAnchors.toggleClass('loznotes__anchor--is-hidden');
+	});
 	
 	// if anything is loaded into the page via ajax, re-create the notes
 	$(document).ajaxComplete(function () {
